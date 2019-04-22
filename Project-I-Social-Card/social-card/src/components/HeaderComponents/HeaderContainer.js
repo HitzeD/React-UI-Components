95f4dc19802200777c34609609c5f1eeabffb895
HitzeD <d-hitze@comcast.net>
2019-04-22 15:27:07 -0500

    finished imagethumbnail and filled out App file

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail.js';
+
+const HeaderContainer = () => {
+    return(
+        <div>
+            <ImageThumbnail />
+
+        </div>
+    );
+}
+
+export default HeaderContainer;
