50a6c715a3feb6b26b494ec69fd8e2180108941e
HitzeD <d-hitze@comcast.net>
2019-04-23 15:56:27 -0500

    finished project

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = props => {
+    return (
+        <div className="action">
+            <p>{props.action}</p>
+        </div>
+    );
+}
+
+export default ActionButton;
