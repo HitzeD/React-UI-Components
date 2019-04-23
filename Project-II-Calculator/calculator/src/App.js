c19889238a4fa51825c0de237374e64267435c3f
HitzeD <d-hitze@comcast.net>
2019-04-23 16:13:23 -0500

    updated styling to make all perfectly lined up

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -12,16 +12,16 @@ const App = () => {
       <NumberButton colorStyle="red" text="/" />
       <NumberButton colorStyle="default" text="7" />
       <NumberButton colorStyle="default" text="8" />
-      <NumberButton colorStyle="default" text="9" />
-      <NumberButton colorStyle="red" text="X" />
+      <NumberButton colorStyle="default lift" text="9" />
+      <NumberButton colorStyle="red push" text="X" />
       <NumberButton colorStyle="default" text="4" />
       <NumberButton colorStyle="default" text="5" />
-      <NumberButton colorStyle="default" text="6" />
-      <NumberButton colorStyle="red" text="-" />
+      <NumberButton colorStyle="default lift" text="6" />
+      <NumberButton colorStyle="red push" text="-" />
       <NumberButton colorStyle="default" text="1" />
       <NumberButton colorStyle="default" text="2" />
-      <NumberButton colorStyle="default" text="3" />
-      <NumberButton colorStyle="red" text="+" />
+      <NumberButton colorStyle="default lift" text="3" />
+      <NumberButton colorStyle="red push" text="+" />
       <ActionButton colorStyle="default" action="0" />
       <NumberButton colorStyle="red" text="=" />
 
